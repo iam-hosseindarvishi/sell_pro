ec5e29bc588962366203df708500367025ce5e6d
Hossein <iam.hosseindarvishi@gmail.com>
2024-05-15 16:33:56 -0400

    feat : init project , create clean architecture structure , create splash screen feature

---
+++ lib/src/core/styles/app_text_style.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'app_font_size.dart';
+
+class AppTextStyle {
+  // -------- black -------//
+
+  static const xSmallBlack = TextStyle(fontSize: AppFontSize.xSmall);
+  static const smallBlack = TextStyle(fontSize: AppFontSize.small);
+  static const mediumBlack =
+      TextStyle(fontSize: AppFontSize.medium, fontWeight: FontWeight.normal);
+  static const largeBlack =
+      TextStyle(fontSize: AppFontSize.large, fontWeight: FontWeight.normal);
+  static const xLargeBlack =
+      TextStyle(fontSize: AppFontSize.xLarge, fontWeight: FontWeight.normal);
+  static const xLargeBlackBold =
+      TextStyle(fontSize: AppFontSize.xLarge, fontWeight: FontWeight.bold);
+  static const xxLargeBlack =
+      TextStyle(fontSize: AppFontSize.xxLarge, fontWeight: FontWeight.normal);
+  static const xxxLargeBlack = TextStyle(
+    fontSize: AppFontSize.xxxLarge,
+    fontWeight: FontWeight.normal,
+  );
+}
