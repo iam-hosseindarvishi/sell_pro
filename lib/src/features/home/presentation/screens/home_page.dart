8af5ce4fa78dd8c0115b6e92d6041e5046baf302
Hossein <iam.hosseindarvishi@gmail.com>
2024-05-31 16:42:13 +0330

    feat : split home list items to widgets

--- lib/src/features/home/presentation/screens/home_page.dart
+++ lib/src/features/home/presentation/screens/home_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sell_pro/src/features/home/presentation/widgets/quick_access_items.dart';
-
-import '../widgets/home_list_access_button.dart';
+import '../widgets/home_list_access_items.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -60,64 +59,7 @@ class _HomePageState extends ConsumerState<HomePage> {
               const SizedBox(
                 height: 15,
               ),
-              Column(
-                children: [
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.blue,
-                    title: "فروش ها",
-                    subTitle: "لیست فروش های ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.red,
-                    title: "خرید ها",
-                    subTitle: "لیست خریدهای ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.green,
-                    title: "دریافتی ها",
-                    subTitle: "لیست دریافتی های ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.yellow,
-                    title: "پرداخت ها",
-                    subTitle: "لیست پرداخت های ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.blue,
-                    title: "فروش ها",
-                    subTitle: "لیست فاکتورهای فروش ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.grey,
-                    title: "برگشت از فروش",
-                    subTitle: "لیست برگشت از فروش های ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.brown,
-                    title: "برگشت از خرید",
-                    subTitle: "لیست برگشت از خریدهای ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.blueGrey,
-                    title: "پیش فاکتورها",
-                    subTitle: "لیست پیش فاکتورهای ثبت شده",
-                  ),
-                  HomeListAccessButton(
-                    handleTab: () {},
-                    borderColor: Colors.blueGrey,
-                    title: "لیست آخرین تغییرات",
-                    subTitle: "لیست اطلاعات ثبت شده،ویرایش شده یا حذف شده",
-                  )
-                ],
-              )
+              const HomeListAccessItems()
             ],
           ),
         ));
--- lib/src/features/home/presentation/widgets/home_list_access_items.dart
+++ lib/src/features/home/presentation/widgets/home_list_access_items.dart
@@ -1,18 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:sell_pro/src/features/home/presentation/widgets/home_quick_access_button.dart';
+
+import 'home_list_access_button.dart';
 
 class HomeListAccessItems extends StatelessWidget {
-  const HomeListAccessItems({super.key});
+  const HomeListAccessItems({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        HomeQuickAccessButton(
-            title: "فروش",
-            subTitle: "لیست فاکتورهای فروش",
-            borderColor: Colors.blue)
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.blue,
+          title: "فروش ها",
+          subTitle: "لیست فروش های ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.red,
+          title: "خرید ها",
+          subTitle: "لیست خریدهای ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.green,
+          title: "دریافتی ها",
+          subTitle: "لیست دریافتی های ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.yellow,
+          title: "پرداخت ها",
+          subTitle: "لیست پرداخت های ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.blue,
+          title: "فروش ها",
+          subTitle: "لیست فاکتورهای فروش ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.grey,
+          title: "برگشت از فروش",
+          subTitle: "لیست برگشت از فروش های ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.brown,
+          title: "برگشت از خرید",
+          subTitle: "لیست برگشت از خریدهای ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.blueGrey,
+          title: "پیش فاکتورها",
+          subTitle: "لیست پیش فاکتورهای ثبت شده",
+        ),
+        HomeListAccessButton(
+          handleTab: () {},
+          borderColor: Colors.blueGrey,
+          title: "لیست آخرین تغییرات",
+          subTitle: "لیست اطلاعات ثبت شده،ویرایش شده یا حذف شده",
+        )
       ],
     );
   }
