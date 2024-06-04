9c7afc9808b5dc39bacf670dc8b9c5a7c1b3225d
Hossein <iam.hosseindarvishi@gmail.com>
2024-06-04 21:40:07 +0330

    fix : fix segment button change state bug

--- lib/src/features/People/presentation/screens/people_screen.dart
+++ lib/src/features/People/presentation/screens/people_screen.dart
@@ -1,15 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class PeopleScreen extends ConsumerWidget {
+class PeopleScreen extends StatefulWidget {
   const PeopleScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Center(
-      child: Text(
-        "People Screen",
-        style: TextStyle(color: Theme.of(context).colorScheme.onBackground),
+  State<PeopleScreen> createState() => _PeopleScreenState();
+}
+
+class _PeopleScreenState extends State<PeopleScreen> {
+  Set<String> selected = {"all"};
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: Center(
+        child: SegmentedButton(
+          style: SegmentedButton.styleFrom(
+              elevation: 5,
+              textStyle: const TextStyle(
+                fontFamily: "IranSans",
+                fontWeight: FontWeight.bold,
+              ),
+              padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 2),
+              disabledBackgroundColor: Colors.grey,
+              disabledForegroundColor: Colors.black,
+              selectedBackgroundColor: Theme.of(context).colorScheme.surface,
+              selectedForegroundColor: Theme.of(context).colorScheme.onSurface,
+              foregroundColor: Theme.of(context).colorScheme.onPrimary,
+              backgroundColor: Theme.of(context).colorScheme.primary),
+          onSelectionChanged: (Set<String> newSelection) {
+            selected = {newSelection.first.toString()};
+            setState(() {});
+          },
+          showSelectedIcon: false,
+          selected: selected,
+          segments: const [
+            ButtonSegment<String>(
+                value: "title",
+                enabled: false,
+                label: Text(
+                  "نمایش",
+                )),
+            ButtonSegment<String>(
+                value: "all",
+                label: Text(
+                  "همه",
+                )),
+            ButtonSegment<String>(
+                value: "costumers",
+                label: Text(
+                  "مشتریان",
+                )),
+            ButtonSegment<String>(
+                value: "sellers",
+                label: Text(
+                  "فروشندگان",
+                )),
+          ],
+        ),
       ),
     );
   }
